b5a01fda8d8ea5c66414711791d17919ce8f91a0
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-09 18:47:28 -0500

    comment added

--- model/db_connection.php
+++ model/db_connection.php
@@ -1,10 +1,13 @@
 <?php
+ //database configuration
  $dsn = 'mysql:host=sql1.njit.edu;dbname=sg948';
  $username = 'sg948';
  $password = 'HfwrZHvX';
- try{
+ 
+ try {
    $db = new PDO($dsn,$username,$password);
- }catch (PDOException $e){
+ }
+ catch (PDOException $e) {
    $error_message = $e->getMessage();
    echo $error_message;
    exit();
