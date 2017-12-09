c7d47a80986f198b0e2a06af1b99ca27515fd337
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-09 18:43:42 -0500

    database connect

---
+++ model/db_connection.php
@@ -0,0 +1,12 @@
+<?php
+ $dsn = 'mysql:host=sql1.njit.edu;dbname=sg948';
+ $username = 'sg948';
+ $password = 'HfwrZHvX';
+ try{
+   $db = new PDO($dsn,$username,$password);
+ }catch (PDOException $e){
+   $error_message = $e->getMessage();
+   echo $error_message;
+   exit();
+ }
+?>
