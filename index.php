aee06ef13cc3cf7cf67a70a6869f2eb7390b448d
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-10 18:27:27 -0500

    homepage setup

---
+++ index.php
@@ -0,0 +1,29 @@
+CTYPE html>
+<html >
+<head>i
+<meta charset="UTF-8">
+  <title>TO DO LIST</title>
+  <style>
+        
+	      @import url(http://fonts.googleapis.com/css?family=Exo:100,200,400);
+	      @import url(http://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
+body{
+	margin: 0;
+	padding: 0;
+	background: #fff;
+        color: #fff;
+	font-family: Arial;
+	font-size: 12px;
+}
+.body{
+	position: absolute;
+	top: -20px;
+	left: -20px;
+	right: -40px;
+	bottom: -40px;
+	width: auto;
+	height: auto;
+	background-image: url(https://static1.squarespace.com/static/5565e32ce4b0dcba1f63442d/t/5660edbbe4b0690830f82514/1449192907245/paper-iphone-to-do-list);
+	background-size: cover;
+	-webkit-filter: blur(0px);
+	z-index: 0;
