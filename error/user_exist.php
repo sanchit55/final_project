ab9baf06dd00fadb6214d7912072f9a6089c32c3
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-09 20:24:29 -0500

    adding statement

---
+++ error/user_exist.php
@@ -0,0 +1,10 @@
+<?php
+echo "<h1> User Name Already Exist</h1><br/>";
+?>
+<html>
+<body>
+<form method = 'post' action='../register.php'>
+<input type="submit" value="Register Page"/>
+</form>
+</body>
+</html>
