6edd44f3f03bfaa7f7bc13335557f3a352325d8d
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-09 20:13:59 -0500

    adding error file

---
+++ error/bad_info.php
@@ -0,0 +1,7 @@
+<div id="main">
+<h1 class="top">Error</h1>
+<p class="first_paragraph">User name or password is incorrect</p>
+</div>
+<form method = 'post' action='../index.php'>
+<input type="submit" value="Login Page"/>
+</form>
