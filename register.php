40e771485673afb647176080fd1dbbb6229eab8b
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-10 18:52:33 -0500

    registration complete

--- register.php
+++ register.php
@@ -154,3 +154,31 @@ body{
 ::-moz-input-placeholder{
    color: rgba(63, 127, 191,0.6);
 }
+</style>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+
+</head>
+
+<body>
+  <div class="body"></div>
+		<div class="grad"></div>
+		
+		<br>
+		<div class="login">
+		<form class="form-login" method="post" action="./task_list/index.php">
+		        <input type="text" placeholder="First Name" name="firstname"><br>
+				<input type="text" placeholder="Last Name" name="lastname"><br>
+				<input type="text" placeholder="Contact No" name="contact"><br>
+				<input type="text" placeholder="E-mail" name="mailid"><br>
+				<input type="text" placeholder="username" name="user"><br>
+				<input type="password" placeholder="password" name="password"><br>
+				<input type ="hidden" name="action" value="register">
+				<input type="submit" value="Sign-Up">
+		</form>
+		</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+  
+</body>
+</html>
