baad9f34fd469e1fe7ad1b36475c48b76e6c17ce
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-09 19:48:35 -0500

    bind value

--- model/db.php
+++ model/db.php
@@ -15,3 +15,13 @@
 	$statement->closeCursor();
 	return true;
    }
+
+   function updateStatus($status,$id){
+        global $db;
+	$query = 'update todo_list set status = :status where id = :id';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':status',$status);
+	$statement->bindValue(':id',$id);
+	$statement->execute();
+	$statement->closeCursor();
+	return true;
