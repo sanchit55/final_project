05a0e778505b0c793dace22bf6ea5cbd50b7816d
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-11 09:56:58 -0500

    adding listfile

---
+++ task_list/list.php
@@ -0,0 +1,29 @@
+<html>
+<style>
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+th, td {
+    font-family: cursive;
+    text-align: left;
+    padding: 8px;
+}
+tr:nth-child(even){background-color: #f2f2f2}
+th {
+    background-color: #4c75af;
+    color: white;
+}
+input[type=text]{
+width: 260px;
+height: 30px;
+background: rgba(255, 255, 255,0.6);
+border: 1px solid rgba(63, 127, 191,0.6);
+border-radius: 2px;
+color: #3F7FBF;
+font-family: 'Exo', sans-serif;
+font-size: 16px;
+font-weight: 400;
+padding: 4px;
+margin-top: 10px;
+}
