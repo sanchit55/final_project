ecfc477c01a7feec9ad6ff3e4aa3aad242dd6ceb
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-11 09:47:17 -0500

    .input adding

--- task_list/edittask.php
+++ task_list/edittask.php
@@ -41,3 +41,32 @@ font-family: cursive;
 color: #fff;
 text-align: right;
 }
+
+.login1{
+position: absolute;
+top: calc(20% - 5px);
+left: calc(55% - 50px);
+background : #fff;
+box-shadow: 5px 5px 3px #2A2B2B;
+z-index : 5;
+color: #4c75af;
+height: 400px;
+width: 300px;
+padding: 10px;
+border-radius: 4px;
+z-index: 2;
+}
+.login{
+position: absolute;
+top: calc(20% - 5px);
+left: calc(30% - 50px);
+background : #fff;
+box-shadow: 5px 5px 3px #2A2B2B;
+z-index : 5;
+color: #4c75af;
+height: 400px;
+width: 300px;
+padding: 10px;
+border-radius: 4px;
+z-index: 2;
+}
