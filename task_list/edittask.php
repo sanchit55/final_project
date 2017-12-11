863908494cc09910b2521717e361937ff6e7d3e8
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-11 09:44:34 -0500

    adding a editfile

---
+++ task_list/edittask.php
@@ -0,0 +1,29 @@
+<?php
+foreach ($result3 as $res3):
+
+endforeach;
+?>
+<html>
+<style>
+body{
+margin: 0;
+padding: 0;
+background: #d3d3d3;
+color: #fff;
+font-family: cursive;
+}
+input[type=submit] {
+width: 120px;
+height: 30px;
+background-color: #4c75af;
+color: white;
+padding: 0px 15px;
+margin: -15px 70px;
+border: none;
+border-radius: 4px;
+cursor: pointer;
+font-family: cursive;
+}
+input[type=submit]:hover {
+background-color: #729edc;
+}
