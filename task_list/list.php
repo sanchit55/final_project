805103e5f45e7c0d1ca72508d3acdfaae7d3cf73
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-11 09:59:21 -0500

    adding statement

--- task_list/list.php
+++ task_list/list.php
@@ -60,3 +60,49 @@ font-family: cursive;
 color: #fff;
 text-align: right;
 }
+
+
+</style>
+  <body style="background: #d3d3d3">
+  <div class="welcome">
+ <h1> To do list system</h1>
+ <strong> <p> Welcome, <?php $fname = $_SESSION['first_name']; echo $fname;?> <?php $lname = $_SESSION['last_name']; echo $lname;?></p></strong>
+  </div>
+   <h3><strong>Task To Complete</strong></h3>
+    <table>
+       <tr>
+      <th style="text-align: center;">Task</th>
+      <th style="text-align: center;">Description</th>
+      <th style="text-align: center;">Date</th>
+      <th style="text-align: center;">Time</th>
+      <th>&nbsp;</th>
+      <th>&nbsp;</th>
+      <th>&nbsp;</th>
+      </tr>
+        <?php foreach($result as $res):?>
+      <tr>
+        <td style="text-align: center;"> <?php echo $res['todo']; ?> </td>
+	<td style="text-align: center;"> <?php echo $res['description']; ?> </td>
+        <td style="text-align: center;"> <?php echo $res['date']; ?>  </td>
+	<td style="text-align: center;"> <?php echo $res['time']; ?> </td>
+
+	<td><form style="margin-top: 15px;" action="index.php" method="post">
+            <input type="hidden" name="user_id" value="<?php echo $res['id']; ?>">
+	    <input style="text-align: center; font-family: cursive;" type="submit" value="Delete">
+	    <input type="hidden" name='action' value="deletetask">
+            </form>
+        </td>
+	<td><form style="margin-top: 15px;" action="index.php" method="post">
+	    <input type="hidden" name="user_id" value="<?php echo $res['id']; ?>">
+	    <input type="submit" style="text-align: center; font-family: cursive;" value="Update Status">
+	    <input type="hidden" name='action' value="statusupdate">
+	    </form>
+        </td>
+        <td>
+	   <form style="margin-top: 15px;" action="index.php" method= "post">
+	   <input type="hidden" name= "user_id" value="<?php echo $res['id']; ?>">
+	   <input type="submit" style="text-align: center;font-family: cursive; " value = "Edit">
+	   <input type="hidden" name='action' value="edittask">
+	   </form>
+	</td>
+</tr>  
