1f281a0fda4a22b2355f3ef218c2ba130ddefef8
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-11 09:37:05 -0500

    adding statement

---
+++ task_list/addtask.php
@@ -0,0 +1,27 @@
+<html>
+<style>
+.welcome{
+background:#4c75af;
+}
+h1 {
+font-family: cursive;
+color: #fff;
+text-align: center;
+}
+h3 {
+font-family: cursive;
+text-align: center;
+}
+p {
+margin-right: 5px;
+font-family: cursive;
+color: #fff;
+text-align: right;
+}
+body{
+margin: 0;
+padding: 0;
+background: #d3d3d3;
+color: #fff;
+font-family: cursive;
+}
