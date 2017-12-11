50e82673491aced6b5d27cdba482a27c5942733f
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-11 10:00:21 -0500

    adding file

--- task_list/list.php
+++ task_list/list.php
@@ -106,3 +106,38 @@ text-align: right;
 	   </form>
 	</td>
 </tr>  
+
+<?php endforeach;?>
+    </table>
+      <form style="margin-top:10px;" method = 'post' action='addtask.php'>
+   <br> <input type="submit" style="font-family: cursive;" value="Add Task"/>
+    </form>
+ <h3><strong>Completed Task</strong></h3>
+    <table>
+           <tr>
+	   <th style="text-align: center;">Task</th>
+	   <th style="text-align: center;">Description</th>
+	   <th style="text-align: center;">Date</th>
+	   <th style="text-align: center;">Time</th>
+	   <th>&nbsp;</th>
+	   </tr>
+	   <?php foreach($result2 as $res2):?>
+	   <tr>
+	   <td style="text-align: center;"> <?php echo $res2['todo']; ?>  </td>
+	   <td style="text-align: center;"> <?php echo $res2['description']; ?>  </td>
+	   <td style="text-align: center;"> <?php echo $res2['date']; ?>  </td>
+           <td style="text-align: center;"> <?php echo $res2['time']; ?>  </td>
+           <td><form style="margin-top: 15px;" action="index.php" method="post">
+               <input type="hidden" name="user_id" value="<?php echo $res2['id']; ?>">
+               <input type="submit" style="font-family: cursive;" value="Delete">
+	       <input type="hidden" name='action' value="deletetask">
+	       </form>
+	  </td>
+          </tr>																									         <?php endforeach;?>
+    </table>
+
+  </body>
+</html>
+<?php
+include('../view/footer.php');
+?>
