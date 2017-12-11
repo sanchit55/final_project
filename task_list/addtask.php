b86aee55f6310419528cf6d693960c7f83a9a2b2
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-11 09:42:21 -0500

    html

--- task_list/addtask.php
+++ task_list/addtask.php
@@ -105,3 +105,23 @@ padding: 10px;
 border-radius: 4px;
 z-index: 2;
 }
+
+</style>
+<body>
+<div class="welcome">
+<h1>Add Task</h1>
+
+</div>
+<div class="login">
+<form style="margin-left:18px;" method = 'post' action='index.php'>
+<strong> Task: </strong><br> <input type='text' name='task'/><br><br>
+<strong> Description: </strong><br> <input type='text' name='description'/><br><br>
+<strong> Date: </strong><br> <input type="date" name = "datetodo"><br><br>
+<strong> Time: </strong><br> <input type="time" name="timetodo"><br><br>
+<input type="hidden" name="userid" value="<?php echo $_COOKIE['my_id']; ?>">
+<input type = 'hidden' name = 'action' value='addtask'><br>
+<input type="submit" value="Add Task"/>
+</form>
+</div>
+</body>
+</html>
