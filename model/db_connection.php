58b2a635516e674674b88936e630e86f17bd4ee4
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-12 10:07:12 -0500

    fixing errors

--- model/db_connection.php
+++ model/db_connection.php
@@ -1,5 +1,24 @@
 <?php
  //database configuration
+ ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+//constants
+define('DATABASE','sg948');
+define('USERNAME','sg948');
+define('PASSWORD','HfwrZHvX');
+define('SERVER','sql1.njit.edu');
+
+
+try {
+    $db = new PDO("mysql:host=" . SERVER . ";dbname=" . DATABASE, USERNAME, PASSWORD);
+    //set PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Successfully connected to the database";
+    }
+    //output error message if connection failed
+catch(PDOException $e) {
+    echo "Unable to connect to the database:" . '<br>' . $e->getMessage();
+    }
  $dsn = 'mysql:host=sql1.njit.edu;dbname=sg948';
  $username = 'sg948';
  $password = 'HfwrZHvX';
