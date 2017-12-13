8ffc2adf5ae66642921ba97d765275ade395b45a
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-12 21:35:32 -0500

    errors

--- register.php
+++ register.php
@@ -166,15 +166,18 @@ body{
 		
 		<br>
 		<div class="login">
-		<form class="form-login" method="post" action="./task_list/index.php">
+		<form class="form-login" method="post" action="https://web.njit.edu/~sg948/finalProject/task_list/index.php">
 		        <input type="text" placeholder="First Name" name="firstname"><br>
 				<input type="text" placeholder="Last Name" name="lastname"><br>
 				<input type="text" placeholder="Contact No" name="contact"><br>
 				<input type="text" placeholder="E-mail" name="mailid"><br>
 				<input type="text" placeholder="username" name="user"><br>
+				<input type="text" placeholder="gender" name="gender"><br>
+				<input type="text" placeholder="birth" name="birth"><br>
 				<input type="password" placeholder="password" name="password"><br>
 				<input type ="hidden" name="action" value="register">
 				<input type="submit" value="Sign-Up">
+
 		</form>
 		</div>
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
