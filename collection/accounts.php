84475a4a4191658e92c3bc3acf3e88dcea7dc115
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-17 16:34:53 -0500

    added accounts&todos

---
+++ collection/accounts.php
@@ -0,0 +1,42 @@
+<?php
+
+class accounts extends \database\collection {
+    protected static $modelName = 'account';
+
+    //This is the function to write to find user by ID for login.
+    
+
+
+    public static function findUserbyEmail($email)
+    {
+
+            $tableName = get_called_class();
+            $sql = 'SELECT * FROM ' . $tableName . ' WHERE email = ?';
+
+         //grab the only record for find one and return as an object
+            $recordsSet = self::getResults($sql, $email);
+
+            if (is_null($recordsSet)) {
+                return FALSE;
+            } else {
+                return $recordsSet[0];
+            }
+    }
+	
+	public static function findUserbyId($id)
+    {
+
+            $tableName = get_called_class();
+            $sql = 'SELECT * FROM ' . $tableName . ' WHERE id = ?';
+
+         //grab the only record for find one and return as an object
+            $recordsSet = self::getResults($sql, $id);
+
+            if (is_null($recordsSet)) {
+                return FALSE;
+            } else {
+                return $recordsSet[0];
+            }
+    }
+}
+a
