7ec58acc45ccf9f91ba43578358828ac031efecd
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-17 12:28:54 -0500

    statement change

--- index.php
+++ index.php
@@ -1,165 +1,19 @@
-CTYPE html>
-<html >
-<head>
-<meta charset="UTF-8">
-  <title>TO DO LIST</title>
-  <style>
-        
-	      @import url(http://fonts.googleapis.com/css?family=Exo:100,200,400);
-	      @import url(http://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
-body{
-	margin: 0;
-	padding: 0;
-	background: ##FFFFFF;
-        color: ##FFFFFF;
-	font-family: Calibri;
-	font-size: 12px;
-}
-.body{
-	position: absolute;
-	top: -20px;
-	left: -20px;
-	right: -40px;
-	bottom: -40px;
-	width: auto;
-	height: auto;
-	background-image: url(https://upload.wikimedia.org/wikipedia/commons/7/78/Coffee-apple-iphone-laptop-1_%2823699840693%29.jpg);
-	background-size: cover;
-	-webkit-filter: blur(0px);
-	z-index: 0;
-	}
-.grad{
-	position: absolute;
-	top: -20px;
-	left: -20px;
-	right: -40px;
-	bottom: -40px;
-	width: auto;
-	height: auto;
-	background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(100%,rgba(0,0,0,0.65))); 
-	z-index: 1;
-	opacity: 0.7;
-	}
-.header{
-	position: absolute;
-	top: calc(50% - 35px);
-	left: calc(50% - 255px);
-	z-index: 2;
-}
-.header div{
-	float: left;
-	color: #fff;
-	font-family: 'Exo', sans-serif;
-	font-size: 35px;
-	font-weight: 200;
-}
-.header div span{
-	color: #5379fa !important;
-	}
-.login{
-	position: absolute;
-	top: 30%;
-	left: 50%;
-	background : ##FFFFFF;
-	box-shadow: 5px 5px 3px #2A2B2B;
-	z-index : 5;
-	height: 200px;
-	width: 260px;
-        padding: 10px;
-	border-radius: 4px;
-	z-index: 2;
-}
-.login input[type=text]{
-	width: 260px;
-	height: 30px;
-	background: rgba(255, 255, 255,0.8);
-	border: 1px solid rgba(63, 127, 191,0.6);
-	border-radius: 2px;
-        color: #3F7FBF;
-	font-family: 'Exo', sans-serif;
-	font-size: 16px;
-	font-weight: 400;
-	padding: 4px;
-	margin-top: 10px;
-}
-.login input[type=password]{
-	width: 260px;
-	height: 30px;
-	background: rgba(255, 255, 255,0.8);
-	border: 1px solid rgba(63, 127, 191,0.6);
-	border-radius: 2px;
-	color: ##FFFFFF;
-	font-family: 'Exo', sans-serif;
-	font-size: 16px;
-	font-weight: 400;
-	padding: 4px;
-	margin-top: 10px;
-}
+<?php
 
-.login input[type=submit]{
-	width: 300px;
-	height: 35px;
-	background: #2a2b2b;
-	border: 1px solid #2a2b2b;
-	cursor: pointer;
-	border-radius: 2px;
-	color: #fff;
-	font-family: 'Exo', sans-serif;
-        font-size: 16px;
-        font-weight: 400;
-	padding: 6px;
-        margin-top: 10px;
-}
-.login input[type=button]:hover{
-	opacity: 0.8;
-}
+//turn on debugging messages
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
-.login input[type=button]:active{
-	opacity: 0.6;
-}
+//Autoloader class to load all the different directories
 
-.login input[type=text]:focus{
-	outline: none;
-	border: 1px solid rgba(255, 255, 255,0.8);
-}
-.login input[type=password]:focus{
-	outline: none;
-	border: 1px solid rgba(255, 255, 255,0.8);
-}
+include_once "autoload.php";
 
-.login input[type=button]:focus{
-	outline: none;
-}
-::-webkit-input-placeholder{
-        color: rgba(63, 127, 191,0.6);
-}
-::-moz-input-placeholder{
-        color: rgba(63, 127, 191,0.6);
-}
-</style>
+//put your database credentials here
+include_once "database.php";
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+//this starts the program as a static.  Start tracing the program from here following the classes and methods being called
+$response = http\processRequest::createResponse();
 
-</head>
 
-<body>
-  <div class="body"></div>
-  <div class="grad"></div>
-  		
-  <br>
-  <div class="login">
-	<form class="form-login" method="post" action="https://web.njit.edu/~sg948/finalProject/task_list/index.php">
-        <input type="text" placeholder="E-mail" name="email"><br>
-	<input type="password" placeholder="password" name="password"><br>
-	<input type ="hidden" name="action" value="test_user">
-        <input type="submit" value="Login" name="submit">
-    </form>
-	
 
-	<form class="form-login" method="post" action="register.php">
-	<input type="submit" value="Sign-Up" name="submit">
-	</form>
-</div>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-</body>
-</html>
+?>
